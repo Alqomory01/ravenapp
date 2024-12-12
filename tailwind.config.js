6a659f92b9b59b99627dc0dffcc3e54518f7b0eb
Alqomory01 <Alqomorytech@gmail.com>
2024-12-12 17:49:43 +0100

    ravenapp

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html',
+],
+  theme: {
+    extend: {},
+    screens: {
+      xl: '1280px',
+      lg: '1024px',
+      md: '768px',
+      sm: '400px',
+      
+     
+     
+    
+    },
+  },
+  plugins: [],
+}
+
